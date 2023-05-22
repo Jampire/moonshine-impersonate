cdfac95ec49d9d8be6241adf4f1e6f525c8ff4c5
Dzianis Kotau <me@dzianiskotau.com>
2023-05-22 16:33:11 +0300

    Added middleware tests (#6)

--- tests/Feature/Http/ImpersonateMiddlewareTest.php
+++ tests/Feature/Http/ImpersonateMiddlewareTest.php
@@ -1,3 +1,73 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
+use Jampire\MoonshineImpersonate\Support\Settings;
+use Jampire\MoonshineImpersonate\Tests\Stubs\Models\MoonshineUser;
+use Jampire\MoonshineImpersonate\Tests\Stubs\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+beforeEach(function (): void {
+    setAuthConfig();
+    enableMoonShineGuard();
+    registerHomePage();
+});
+
+it('authorizes impersonator as impersonated user', function (): void {
+    $name = 'John Doe';
+    $user = User::factory()->create([
+        'name' => $name,
+    ]);
+    $moonShineUser = MoonshineUser::factory()->create();
+
+    $response = get(route('test.me'));
+
+    $response->assertOk();
+
+    expect($response->content())
+        ->toBe('Not authorized')
+    ;
+
+    actingAs($moonShineUser, Settings::moonShineGuard());
+    post(route_impersonate('enter'), [
+        'id' => $user->id,
+    ])->assertSessionHasNoErrors();
+
+    $response = get(route('test.me'));
+
+    $response->assertOk();
+
+    expect($response->content())
+        ->toBe($name)
+    ;
+});
+
+it('cannot impersonate when admin is not authorized', function (): void {
+    Event::fake();
+
+    $user = User::factory()->create();
+    $moonShineUser = MoonshineUser::factory()->create();
+
+    actingAs($moonShineUser, Settings::moonShineGuard());
+    post(route_impersonate('enter'), [
+        'id' => $user->id,
+    ])->assertSessionHasNoErrors();
+
+    Auth::logout();
+
+    expect(session()->get(Settings::key()))
+        ->toBe($user->getKey());
+
+    $response = get(route('test.me'));
+
+    $response->assertOk();
+
+    expect($response->content())
+        ->toBe('Not authorized')
+    ;
+});
